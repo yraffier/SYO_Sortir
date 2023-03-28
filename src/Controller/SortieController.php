cbeacc40f278198006e7cbb342e1ffcfabf3f0d2
esmOnnur <onur.esmer2022@campus-eni.fr>
2023-03-28 14:39:20 +0200

    Mise en place de la page acceuil utilisateur, twig ajouter/et detail implementation des métehodes dans le controller Sortie

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
 use App\Repository\SortieRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use PHPUnit\Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,16 +18,52 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class SortieController extends AbstractController
 {
-    #[Route('/', name: 'sortie_lister')]
+    #[Route('/', name: '_lister')]
     public function lister(
         SortieRepository $sortieRepository
     ): Response
     // Méthodes pour récupérer l'ensemble des sorties
     {
         $sorties= $sortieRepository->findAll();
-        return $this->render('sortie/lister.html.twig', compact('sorties')
+        return $this->render('sortie/accueilUtilisateur.html.twig', compact('sorties')
        );
-
+    }
+    /*
+     * Méthode pour se descinscrir d'une sortie
+     */
+    #[Route('/sedesincrire/{sortie}', name: '_sedesinscrire')]
+    public function sedesinscrire(
+        Sortie $sortie,
+        EntityManagerInterface $entityManager,
+        SortieRepository $sortieRepository
+    ): Response
+    {
+       try{
+        $entityManager->remove($sortie);
+        $entityManager->flush();
+        $this->addFlash('succes','Vous êtes bien déscincrits de la sortie');
+        return $this->redirectToRoute('sortie_lister');
+       }catch(\Exception $exception) {
+           $this->addFlash('echec', 'Vous n\'êtes pas désincrits de la sortie');
+           return $this->redirectToRoute('sortie_lister');
+           }
 
     }
+    /*
+     * Méthode pour afficher le déatails d'un sortie avec l'id de la sortie
+     */
+    #[Route('/detail/{sortie}', name: '_detail')]
+    public function detail(
+        Sortie  $sortie,
+        SortieRepository $sortieRepository
+    ): Response
+       {
+        if(!$sortie){
+            throw $this->createNotFoundException('Cette sortie n\'existe pas');
+               }
+        return $this->render('sortie/detail.html.twig', compact('sortie'));
+       }
+
+
+
 }

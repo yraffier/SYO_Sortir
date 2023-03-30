7e857835a25ddd2a0e294ffd4a084062ca9c2572
sasha <sasha.laigle2022@campus-eni.fr>
2023-03-30 12:54:05 +0200

    :ambulance: Recuperation de la bonne version de AjouterSortieType + Minor fix

--- src/Form/AjouterSortieType.php
+++ src/Form/AjouterSortieType.php
@@ -2,10 +2,9 @@
 
 namespace App\Form;
 
-use App\Entity\Campus;
 use App\Entity\Sortie;
 use App\Entity\Lieu;
-use App\Entity\Ville;
+use App\Entity\Campus;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
@@ -13,6 +12,7 @@ use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
+
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AjouterSortieType extends AbstractType
@@ -21,57 +21,55 @@ class AjouterSortieType extends AbstractType
     {
         $builder
             ->add('nom')
-            ->add('dateHeureDebut',DateTimeType::class,
+            ->add('dateHeureDebut', DateTimeType::class,
                 [
-                'label'=>"date de l'activité",
-                'widget'=>'single_text',
-                ]
-            )
-            ->add('duree',TimeType::class,[
-                'input'=>'timestamp'
-            ])
-            ->add('dateLimiteInscription',DateTimeType::class,[
-                'widget'=>'single_text',
-            ])
-            ->add('infosSortie',TextareaType::class,[
-                'attr'=>['rows'=>5],
-            ])
-            ->add('nbInscriptionMax',NumberType::class,[
-                'attr'=>['min'=>1],
-            ])
-            ->add('lieu',EntityType::class,
+                    'label' => "Date de l'activité : ",
+                    'widget' => 'single_text',
+                    'html5'=>true,
+                ])
+            ->add('dateLimiteInscription', DateTimeType::class,
+                [
+                    'label' => "Date limite d'inscription : ",
+                    'widget' => 'single_text',
+                    'html5'=>true
+                ])
+            ->add('duree', TimeType::class,
+                [
+                    'label' => "Durée : ",
+                    'input' => 'timestamp'
+                ])
+            ->add('infosSortie', TextareaType::class,
+                [
+                    'label' => "Description et infos : ",
+                    'attr' => ['rows' => 3]
+                ])
+            ->add('nbInscriptionMax', NumberType::class,
+                [
+                    'label' => "Nombre de places : ",
+                    'attr' => ['min' => 1]
+                ])
+            ->add('siteOrganisateur', EntityType::class,
+                [
+                    'label' => "Campus : ",
+                    'class' => Campus::class,
+                    'choice_label' => 'nom'
+                ])
+            ->add('lieu', EntityType::class,
                 [
                     'label' => "Lieu : ",
                     'class' => Lieu::class,
                     'required' => true,
-                   'choice_label' => function(Lieu $lieu){
-                    return $lieu->getNom().'-'.$lieu->getRue().'-'.$lieu->getVille()->getCodePostal();
-                   }]
-//                // TODO choicelabel: choice label est un array, et si le choice label est un objet lieu
-//                    'choice_label' => new Lieu()
-            ])
-            ->add('Ville',EntityType::class,
-                [
-                    'class' => Ville::class,
-                    'label' => function(Ville $ville){
-                        return $ville->getNom();
-                        ]
-                    }
-            [
-            'class'=> Campus::class,
-            'choice_label'=> 'nom',
-            ]
-    )
-//            ->add('etat')
-           // ->add('participants')
-            //->add('organisateurs')
-        ;
+                    'multiple' => false,
+                    'choice_label' => function (Lieu $lieu) { return $lieu->getNom().'-'.$lieu->getRue().'-'.$lieu->getVille()->getCodePostal(); }
+
+                ]);
+
     }
 
     public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([
-            'data_class' => Sortie::class,
-        ]);
-    }
+        {
+            $resolver->setDefaults([
+                'data_class' => Sortie::class,
+            ]);
+        }
 }

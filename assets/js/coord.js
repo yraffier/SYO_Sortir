64639c647c59afa46a1a53dd9c83a3752de89f6c
sasha <sasha.laigle2022@campus-eni.fr>
2023-04-14 10:24:54 +0200

    :coffin: Suppression des consoles.log et des commentaires inutiles dans le fichier coord.js

--- assets/js/coord.js
+++ assets/js/coord.js
@@ -2,9 +2,9 @@ $("#inputRue").keyup(function(event) {
     // Stop la propagation par défaut
     event.preventDefault();
     event.stopPropagation();
-
+    // Récupération de la value rue
     let rue = $("#inputRue").val();
-    console.log(rue);
+    //La requête API demande 3 lettres minimum pour rechercher
     if (rue.length >= 3) {
         $.get('https://api-adresse.data.gouv.fr/search/', {
             q: rue,
@@ -13,25 +13,17 @@ $("#inputRue").keyup(function(event) {
         }, function (data) {
             let liste = "";
             $.each(data.features, function(i, obj) {
-                // console.log(obj.properties);
-                // console.log(obj.geometry.coordinates);
-                // données phase 1 (obj.properties.label) & phase 2 : name, postcode, city
                 // J'ajoute chaque élément dans une liste
                 liste += '<li><a href="#" name="'+obj.properties.label+'" data-name="'+obj.properties.name+'" data-postcode="'+obj.properties.postcode+'" data-city="'+obj.properties.city+'" data-lat="'+obj.geometry.coordinates[1]+'" data-long="'+obj.geometry.coordinates[0]+'">'+obj.properties.label+'</a></li>';
-                // liste += '<li><a href="#" name="'+obj.properties.label+'" data-name="'+obj.properties.name+'" data-postcode="'+obj.properties.postcode+'" data-city="'+obj.properties.city+'" data-lat="'+obj.geometry.coordinates[0]+'" data-long="'+obj.geometry.coordinates[1]+'>'+obj.properties.label+'</a></li>';
-                // liste += '<li><a href="#" name="'+obj.properties.label+'" data-name="'+obj.properties.name+'" data-postcode="'+obj.properties.postcode+'" data-city="'+obj.properties.city+'">'+obj.properties.label+'</a></li>';
-                console.log(liste);
             });
             $('.adress-feedback ul').html(liste);
 
-            // ToDo: Au clic du lien voulu, on envoie l'info en $_POST
             $('.adress-feedback ul>li').on("click","a", function(event) {
                 // Stop la propagation par défaut
                 event.preventDefault();
                 event.stopPropagation();
 
                 // let adresse = $(this).attr("name");
-
                 $("#inputRue").val($(this).attr("data-name"));
                 $("#inputCodePostal").val($(this).attr("data-postcode"));
                 $("#inputVille").val($(this).attr("data-city"));
